2874d02c3897be2bd96be4e319abb435a38bc5bf
sagar23s <87541563+sagar23s@users.noreply.github.com>
2021-07-17 11:40:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+     var principal = document.getElementById("principal").value; 
+     var years = document.getElementById("years").value;
+     var rate = document.getElementById("rate").value;
+     var future = +years + 2021;
+     var int = principal * years * rate/100;
     
+                if (principal <= 0) {
+                    alert("Please enter positive value");
+                    document.getElementById("principal").focus();
+                    }
+                    
+                    else{    
+                    interest =document.getElementById("result"); 
+               interest.innerHTML = "If you deposit " + (principal) + "," + ("<br>") + "at an interest rate of " + (rate) + "%." + ("<br>") + "You will receive an amount of " + (int) + "," +("<br>") + "in the year " + (future);
+                    }
+            
 }
-        
